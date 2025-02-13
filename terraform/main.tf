8d89951245d8aad9e27a7ce443117b8a144f484d
haha93 <balu.sasi93@gmail.com>
2025-02-13 17:11:43 +0530

    Add multiple files

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+resource "google_sql_database_instance" "instance" {
+  name             = var.instance_name
+  database_version = "MYSQL_8_0"  # or your preferred version
+  region           = var.region
+  
+  settings {
+    tier = "db-f1-micro"  # or your preferred tier
+    
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+    }
+    
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "allow-all"
+        value = "0.0.0.0/0"  # Be more restrictive in production
+      }
+    }
+  }
+
+  deletion_protection = false  # Set to true for production
+}
+
+resource "google_sql_database" "database" {
+  name     = var.database_name
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_user" "users" {
+  name     = var.database_user
+  instance = google_sql_database_instance.instance.name
+  password = var.database_password
+}
